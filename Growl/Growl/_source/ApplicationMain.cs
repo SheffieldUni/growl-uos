78e14e2609e75cc3daa9e911b0c7e6bfed9cf2a4
Tim Parkinson <t.r.parkinson@sheffield.ac.uk>
2013-04-25 10:35:11 +0100

    added nogui cmd line param

--- Growl/Growl/_source/ApplicationMain.cs
+++ Growl/Growl/_source/ApplicationMain.cs
@@ -24,6 +24,7 @@ namespace Growl
         static Program program;
         static bool appIsAlreadyRunning;
         static bool silentMode;
+        static bool noguiMode;
         static bool loggingEnabled;
         static bool showSettingsOnLaunch;
         static List<InternalNotification> queuedNotifications = new List<InternalNotification>();
@@ -158,6 +159,12 @@ namespace Growl
                             if (silentMode)
                                 signalFlag = signalFlag | Signal.Silent;
                         }
+                        if (parameters.ContainsKey("/nogui"))
+                        {
+                            string nogui = parameters["/nogui"].Value.ToLower();
+                            if (nogui == "true") noguiMode = true;
+
+                        }
                         string listenUrlFile = null;
                         if (parameters.ContainsKey("/listenurl")) listenUrlFile = parameters["/listenurl"].Value;
                         else if (args != null && args.Length == 1) listenUrlFile = args[0];
@@ -238,6 +245,18 @@ namespace Growl
             set
             {
                 silentMode = value;
+            }
+        }
+
+        static public bool NoguiMode
+        {
+            get
+            {
+                return noguiMode;
+            }
+            set
+            {
+                noguiMode = value;
             }
         }
 
